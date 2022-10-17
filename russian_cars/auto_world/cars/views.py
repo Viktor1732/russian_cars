4502adb2444386ffb0b0f8b4101264b048ee29e7
Viktor1732 <programmer.kras@gmail.com>
2022-10-17 22:01:04 +0700

    1. Прописаны маршруты. 2. Прописаны пробные функции. 3. Добавлена функция pageNotFound для обработки исключения.

--- russian_cars/auto_world/cars/views.py
+++ russian_cars/auto_world/cars/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('Главная страница приложения cars')
+
+
+def categories(request):
+    return HttpResponse('<h1>Статьи по категориям</h1>')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
