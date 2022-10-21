932de3f24ab4f55b265686fdbdcd23108c4a853b
Viktor1732 <programmer.kras@gmail.com>
2022-10-21 22:02:52 +0700

    1. Добавлены пути к медиа-файлам. 2. Добавлена модель Cars.

--- russian_cars/auto_world/auto_world/urls.py
+++ russian_cars/auto_world/auto_world/urls.py
@@ -13,14 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from auto_world import settings
 from cars.views import pageNotFound
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('cars.urls')),
 ]
+
+# Путь к графическим данным в режиме отладки.
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = pageNotFound
