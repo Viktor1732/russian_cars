acee419d03a7b23667e00179db5b3a29c98b6ea7
Viktor1732 <programmer.kras@gmail.com>
2022-10-21 21:34:20 +0700

    Добавил категории к маршрутам.

--- russian_cars/auto_world/cars/views.py
+++ russian_cars/auto_world/cars/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
 
 
 def index(request):
     return HttpResponse('Главная страница приложения cars')
 
 
-def categories(request):
-    return HttpResponse('<h1>Статьи по категориям</h1>')
+def categories(request, catid):
+    return HttpResponse(f'<h1>Статьи по категориям</h1><p>{catid}</p>')
 
 
 def pageNotFound(request, exception):
