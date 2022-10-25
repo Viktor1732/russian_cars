c2a8db90123f6c6a8db88470c15c9b70ea4f74a3
Viktor1732 <programmer.kras@gmail.com>
2022-10-25 22:03:00 +0700

    Настроил админ-панель.

--- russian_cars/auto_world/cars/admin.py
+++ russian_cars/auto_world/cars/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+#Для добавления дополнительных полей в амин-панели.
+class CarsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'time_create', 'photo', 'is_published')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content') #Добавляется строка поиска по заголовку и по контенту.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+#Для регистрации в админ-панели.
+admin.site.register(Cars, CarsAdmin)
+admin.site.register(Category, CategoryAdmin)
