3b022356e70edd26f3726822eb2847d4ac862cc5
Viktor1732 <programmer.kras@gmail.com>
2022-10-27 21:56:22 +0700

    Создал и подключил включающий тег для отображения контента по категориям.

--- russian_cars/auto_world/cars/templatetags/cars_tags.py
+++ russian_cars/auto_world/cars/templatetags/cars_tags.py
@@ -6,6 +6,17 @@ register = template.Library()  # через экземпляр Library прох�
 
 
 @register.simple_tag(name='getcats')
-def get_categories():
-    return Category.objects.all()
+def get_categories(filter=None):
+    if not filter:
+        return Category.objects.all()
+    else:
+        return Category.objects.filter(pk=filter)
 
+
+@register.inclusion_tag('cars/list_categories.html')
+def show_categories(sort=None, cat_selected=0):
+    if not sort:
+        cats = Category.objects.all()
+    else:
+        cats = Category.objects.order_by(sort)
+    return {'cats': cats, 'cat_selected': cat_selected}
