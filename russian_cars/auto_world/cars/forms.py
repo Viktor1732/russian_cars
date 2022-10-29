21f406108b3ac3915d37948f1e5961d31dd557ca
Viktor1732 <programmer.kras@gmail.com>
2022-10-30 00:33:57 +0700

    - Создал собственный валидатор для title.

--- russian_cars/auto_world/cars/forms.py
+++ russian_cars/auto_world/cars/forms.py
@@ -1,19 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import *
 
 
 class AddPostForm(forms.ModelForm):
-    #Для того, чтобы отображалось при выборе категории "категория не выбрана".
+    # Для того, чтобы отображалось при выборе категории "категория не выбрана".
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['cat'].empty_label = 'Категория не выбрана'
 
     class Meta:
         model = Cars
-        fields = ['title', 'slug', 'content', 'photo' , 'is_published', 'cat']
-        #Прописываю индивидуальные стили для полей.
+        fields = ['title', 'slug', 'content', 'photo', 'is_published', 'cat']
+        # Прописываю индивидуальные стили для полей.
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-input'}),
             'content': forms.Textarea(attrs={'cols': 60, 'rows': 10}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if len(title) > 50:
+            raise ValidationError('Длина превышает 50 символов')
+        return title
