08f7b09820c718d0bc40431d3787fd08310d8fa5
Viktor1732 <programmer.kras@gmail.com>
2022-11-05 20:45:54 +0700

    - Кэшировал часть кода с помощью IP низкого уровня.

--- russian_cars/auto_world/cars/utils.py
+++ russian_cars/auto_world/cars/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Count
+from django.core.cache import cache
 
 from cars.models import Category
 
@@ -15,7 +16,10 @@ class DataMixin:
 
     def get_user_context(self, **kwargs):
         context = kwargs
-        cats = Category.objects.annotate(Count('cars'))
+        cats = cache.get('cats')
+        if not cats:
+            cats = Category.objects.annotate(Count('cars'))
+            cache.set('cats', cats, 60)
 
         user_menu = menu.copy()
         if not self.request.user.is_authenticated:
